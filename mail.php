50c4d55acd91d7bfa78cf6b60d383d938763a6c7
dzatikyan <dianaz1995@gmail.com>
2021-12-26 14:19:50 -0800

    Update mail.php
    New mail form code

--- mail.php
+++ mail.php
@@ -1,20 +1,48 @@
+
 <?php
-    session_start();
-    if( isset($_POST['name']) && strtoupper($_POST['captcha']) == $_SESSION['captcha_id'] )
-    {
-        $to = 'diana@dzatikyan.com'; // Replace with your email   
-        $subject = 'Message from UX Portfolio'; // Replace with your $subject
-        $headers = 'From: ' . $_POST['email'] . "\r\n" . 'Reply-To: ' . $_POST['email'];    
 
-        $message = 'Name: ' . $_POST['name'] . "\n" .
-                   'E-mail: ' . $_POST['email'] . "\n" .
-                   'Subject: ' . $_POST['subject'] . "\n" .
-                   'Message: ' . $_POST['message'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        mail($to, $subject, $message, $headers);    
-        if( $_POST['copy'] == 'on' )
-        {
-            mail($_POST['email'], $subject, $message, $headers);
+        # FIX: Replace this email with recipient email
+        $mail_to = "dianaz1995@gmail.com";
+        
+        # Sender Data
+        // $subject = trim($_POST["subject"]);
+        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
+        
+        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
+            # Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Please complete the form and try again.";
+            exit;
         }
+        
+        # Mail Content
+        $content = "Name: $name\n";
+        $content .= "Email: $email\n\n";
+        $content .= "Message:\n$message\n";
+
+        # email headers.
+        $headers = "From: $name <$email>";
+
+        # Send the email.
+        $success = mail($mail_to,$content, $headers);
+        if ($success) {
+            # Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            # Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong, we couldn't send your message.";
+        }
+
+    } else {
+        # Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
     }
-        ?>
+
+?>
